320841b4ddd0bd5a0eec3de8a939c42a56feafd1
onurdemirk <96961148+onurdemirk@users.noreply.github.com>
2025-01-23 21:48:37 +0100

    Optimized task iteration by mapping tasks into a flat array for easier manipulation. Added support for dynamic project selection when a new project is added.

--- src/project.js
+++ src/project.js
@@ -1,3 +1,5 @@
+import { formatTaskDate, tasksInWeek, tasksInDay } from "./date.js";
+
 export default class Project {
   constructor(projectName) {
     this.projectName = projectName;
@@ -18,6 +20,7 @@ allProjects.push(project1, project2, project3);
 
 allProjects[0].tasks.push({
   projectId: allProjects[0].id,
+  taskId :"task001",
   title: "Learn Javascript",
   detail: "OOP",
   dueDate: "2025-01-31",
@@ -28,25 +31,31 @@ allProjects[0].tasks.push({
 allProjects[1].tasks.push({
   projectId: allProjects[1].id,
   title: "Study English",
+  taskId :"task002",
   detail: "Read Essays.",
   dueDate: "2025-02-15",
   priority: "medium",
-  isDone: true,
+  isDone: false,
 });
 
 allProjects[1].tasks.push({
   projectId: allProjects[1].id,
+  taskId : "task003",
   title: "Study German",
   detail: "Check the future tense.",
   dueDate: "2025-01-30",
   priority: "low",
-  isDone: true,
+  isDone: false,
 });
 
-// TEST 
 
 export function getProjectById(projectId) {
   return allProjects.find((project) => project.id === projectId);
+}
+
+export function getProjectNameById(projectId) {
+  const project = getProjectById(projectId);
+  return project ? project.projectName : null;
 }
 
 export function displayProjects() {
@@ -56,15 +65,37 @@ export function displayProjects() {
   allProjects.forEach((project) => {
     const projectBlock = document.createElement("div");
     projectBlock.className = "my-projects-block";
-    projectBlock.setAttribute("data-id", project.id); 
+    projectBlock.setAttribute("data-project-id", project.id); 
 
     const projectName = document.createElement("p");
     projectName.textContent = `${project.projectName}`;
     projectName.className = "project-name";
+    projectName.setAttribute("data-project-id", project.id);
 
     const taskNumber = document.createElement("p");
-    taskNumber.textContent = project.tasks.length;
+
+    const incompleteTasks = project.tasks.filter((task) => !task.isDone);
+    taskNumber.textContent = incompleteTasks.length;
+
+    const allTasks = document.getElementById("home-number");
+    allTasks.innerHTML = allProjects.reduce((count, project) => {
+      return count + project.tasks.filter((task) => !task.isDone).length;
+    }, 0);
+
+    const weekTasks = document.getElementById("week-number");
+    weekTasks.innerHTML = allProjects.reduce((count, project) => {
+      const incompleteTasksInWeek = tasksInWeek(project.tasks).filter((task) => !task.isDone);
+      return count + incompleteTasksInWeek.length;  
+    }, 0);
+
+    const todayTasks = document.getElementById("today-number");
+    todayTasks.innerHTML = allProjects.reduce((count, project) => {
+      const incompleteTasksInDay = tasksInDay(project.tasks).filter((task) => !task.isDone);
+      return count + incompleteTasksInDay.length;  
+    }, 0);
+
     taskNumber.className = "project-number";
+    taskNumber.setAttribute("data-project-id", project.id); 
 
     projectBlock.appendChild(projectName);
     projectBlock.appendChild(taskNumber);
