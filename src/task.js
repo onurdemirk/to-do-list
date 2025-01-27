df161a7b54bd79d25f28f23b28a7761de5ec17a9
onurdemirk <96961148+onurdemirk@users.noreply.github.com>
2025-01-27 20:56:32 +0100

    Provide functions to display tasks in different scopes
    - displayTasks(projectId): Show tasks for a selected project in date order.
    - displayTasksAll(): List tasks from all projects.
    - displayTasksInWeek()/displayTasksInDay(): Filter tasks for the upcoming week or current day.
    - Create task HTML elements with checkboxes, edit, and delete icons.

--- src/task.js
+++ src/task.js
@@ -1,37 +1,22 @@
-import { getProjectById, allProjects } from "./project.js";
+import { getProjectById } from "./project.js";
 import { formatTaskDate, tasksInWeek, tasksInDay } from "./date.js";
 import imgEdit from "./img/pen.png";
 import imgDelete from "./img/delete.png";
-
-export default class Task {
-  constructor(projectId, taskId, title, detail, dueDate, priority, isDone = false) {
-    this.projectId = projectId;
-    this.taskId = taskId
-    this.title = title;
-    this.detail = detail;
-    this.dueDate = dueDate;
-    this.priority = priority;
-    this.isDone = isDone;
-  }
-}
+import { getProjectsFromLocalStorage } from "./local-storage.js";
 
 export function displayTasks(projectId) {
-
   const tasksContainer = document.querySelector(".task-container");
   tasksContainer.innerHTML = "";
 
   const selectedProject = getProjectById(projectId);
 
   if (selectedProject && selectedProject.tasks.length > 0) {
-    
-    const sortedTasks = [...selectedProject.tasks].sort((a,b) => {
-
+    const sortedTasks = [...selectedProject.tasks].sort((a, b) => {
       const dateA = new Date(a.dueDate).getTime();
       const dateB = new Date(b.dueDate).getTime();
       return dateA - dateB;
-    })
-    
-    
+    });
+
     sortedTasks.forEach((task) => {
       const taskMenu = document.createElement("div");
       taskMenu.className = "task-menu";
@@ -50,12 +35,20 @@ export function displayTasks(projectId) {
       taskMenu.style.borderLeft = `5px solid ${priorityColor}`;
 
       taskMenu.innerHTML = `<div class="task-first-block">
-              <input type="checkbox" id="task-done" class="task-done" name="task-done"  task-id="${task.taskId}"
-                data-project-id="${projectId}" />
-              <p class="task-title">${task.title}</p>
+              <input type="checkbox" id="task-done" class="task-done ${
+                task.isDone ? "checked" : ""
+              }" name="task-done"  task-id="${task.taskId}"
+                data-project-id="${projectId}" ${
+        task.isDone ? "checked" : ""
+      } />
+              <p class="task-title ${task.isDone ? "checked" : ""}">${
+        task.title
+      }</p>
             </div>
             <div class="task-second-block">
-              <p class="task-details" task-id="${task.taskId}" data-project-id="${projectId}">DETAILS</p>
+              <p class="task-details" task-id="${
+                task.taskId
+              }" data-project-id="${projectId}">DETAILS</p>
               <p class="task-date">${formatTaskDate(task.dueDate)}</p>
               <img
                 src="${imgEdit}"
@@ -78,6 +71,10 @@ export function displayTasks(projectId) {
                </div>
             `;
 
+      if (task.isDone) {
+        taskMenu.classList.add("checked");
+      }
+
       tasksContainer.appendChild(taskMenu);
     });
   } else {
@@ -91,9 +88,10 @@ export function displayTasksAll() {
 
   const allTasksArray = [];
 
-  allProjects.forEach((project) => {
-    project.tasks.forEach((task) => {
+  const projects = getProjectsFromLocalStorage();
 
+  projects.forEach((project) => {
+    project.tasks.forEach((task) => {
       allTasksArray.push({
         ...task,
         projectId: project.id,
@@ -108,7 +106,94 @@ export function displayTasksAll() {
   });
 
   sortedAllTasks.forEach((task) => {
+    const taskMenu = document.createElement("div");
+    taskMenu.className = "task-menu";
+    taskMenu.setAttribute("data-project-id", task.projectId);
+    taskMenu.setAttribute("task-id", task.taskId);
 
+    let priorityColor = "gray";
+    if (task.priority === "high") {
+      priorityColor = "red";
+    } else if (task.priority === "medium") {
+      priorityColor = "orange";
+    } else if (task.priority === "low") {
+      priorityColor = "green";
+    }
+
+    taskMenu.style.borderLeft = `5px solid ${priorityColor}`;
+
+    taskMenu.innerHTML = `<div class="task-first-block">
+              <input type="checkbox" id="task-done" class="task-done ${
+                task.isDone ? "checked" : ""
+              }" name="task-done"  task-id="${task.taskId}"
+                data-project-id="${task.projectId}" ${
+      task.isDone ? "checked" : ""
+    } />
+              <p class="task-title ${task.isDone ? "checked" : ""}">${
+      task.title
+    }</p>
+            </div>
+            <div class="task-second-block">
+              <p class="task-details" task-id="${
+                task.taskId
+              }" data-project-id="${task.projectId}">DETAILS</p>
+              <p class="task-date">${formatTaskDate(task.dueDate)}</p>
+              <img
+                src="${imgEdit}"
+                id="task-edit"
+                class="button"
+                alt="task-edit"
+                task-id="${task.taskId}"
+                data-project-id="${task.projectId}"
+                width="20px"
+              />
+              <img
+                src="${imgDelete}"
+                id="task-delete"
+                class="button"
+                alt="task-delete"
+                task-id="${task.taskId}"
+                data-project-id="${task.projectId}"
+                width="20px"
+              />
+               </div>
+            `;
+
+    if (task.isDone) {
+      taskMenu.classList.add("checked");
+    }
+
+    tasksContainer.appendChild(taskMenu);
+  });
+}
+
+export function displayTasksInWeek() {
+  const tasksContainer = document.querySelector(".task-container");
+  tasksContainer.innerHTML = "";
+
+  const allWeekTasksArray = [];
+
+  const projects = getProjectsFromLocalStorage();
+
+  projects.forEach((project) => {
+    const weeklyTasks = tasksInWeek(project.tasks);
+
+    weeklyTasks.forEach((task) => {
+      allWeekTasksArray.push({
+        ...task,
+        projectId: project.id,
+      });
+    });
+  });
+
+  const sortedWeekAllTasks = allWeekTasksArray.sort((a, b) => {
+    const dateA = new Date(a.dueDate).getTime();
+    const dateB = new Date(b.dueDate).getTime();
+    return dateA - dateB;
+  });
+
+  if (sortedWeekAllTasks.length > 0) {
+    sortedWeekAllTasks.forEach((task) => {
       const taskMenu = document.createElement("div");
       taskMenu.className = "task-menu";
       taskMenu.setAttribute("data-project-id", task.projectId);
@@ -126,90 +211,20 @@ export function displayTasksAll() {
       taskMenu.style.borderLeft = `5px solid ${priorityColor}`;
 
       taskMenu.innerHTML = `<div class="task-first-block">
-              <input type="checkbox" id="task-done" class="task-done" name="task-done"  task-id="${task.taskId}"
-                data-project-id="${task.projectId}" />
-              <p class="task-title">${task.title}</p>
-            </div>
-            <div class="task-second-block">
-              <p class="task-details" task-id="${task.taskId}" data-project-id="${
-                task.projectId
-      }">DETAILS</p>
-              <p class="task-date">${formatTaskDate(task.dueDate)}</p>
-              <img
-                src="${imgEdit}"
-                id="task-edit"
-                class="button"
-                alt="task-edit"
-                task-id="${task.taskId}"
-                data-project-id="${task.projectId}"
-                width="20px"
-              />
-              <img
-                src="${imgDelete}"
-                id="task-delete"
-                class="button"
-                alt="task-delete"
-                task-id="${task.taskId}"
-                data-project-id="${task.projectId}"
-                width="20px"
-              />
-               </div>
-            `;
-
-      tasksContainer.appendChild(taskMenu);
-    });
-  }
-
-export function displayTasksInWeek() {
-  const tasksContainer = document.querySelector(".task-container");
-  tasksContainer.innerHTML = "";
-
-  const allWeekTasksArray = [];
-
-  allProjects.forEach((project) => {
-    const weeklyTasks = tasksInWeek(project.tasks);
-
-    weeklyTasks.forEach((task) => {
-      allWeekTasksArray.push({
-        ...task,
-        projectId: project.id,
-      });
-    });
-    });
-
-    const sortedWeekAllTasks = allWeekTasksArray.sort((a, b) => {
-      const dateA = new Date(a.dueDate).getTime();
-      const dateB = new Date(b.dueDate).getTime();
-      return dateA - dateB;
-    });
-
-    if (sortedWeekAllTasks.length > 0) {
-      sortedWeekAllTasks.forEach((task) => {
-        const taskMenu = document.createElement("div");
-        taskMenu.className = "task-menu";
-        taskMenu.setAttribute("data-project-id", task.projectId);
-        taskMenu.setAttribute("task-id", task.taskId);
-
-        let priorityColor = "gray";
-        if (task.priority === "high") {
-          priorityColor = "red";
-        } else if (task.priority === "medium") {
-          priorityColor = "orange";
-        } else if (task.priority === "low") {
-          priorityColor = "green";
-        }
-
-        taskMenu.style.borderLeft = `5px solid ${priorityColor}`;
-
-        taskMenu.innerHTML = `<div class="task-first-block">
-                <input type="checkbox" id="task-done" class="task-done" name="task-done"  task-id="${task.taskId}"
-                  data-project-id="${task.projectId}" />
-                <p class="task-title">${task.title}</p>
+                <input type="checkbox" id="task-done" class="task-done ${
+                  task.isDone ? "checked" : ""
+                }" name="task-done"  task-id="${task.taskId}"
+                  data-project-id="${task.projectId}" ${
+        task.isDone ? "checked" : ""
+      } />
+                <p class="task-title ${task.isDone ? "checked" : ""}">${
+        task.title
+      }</p>
               </div>
               <div class="task-second-block">
-                <p class="task-details" task-id="${task.taskId}" data-project-id="${
-                  task.projectId
-        }">DETAILS</p>
+                <p class="task-details" task-id="${
+                  task.taskId
+                }" data-project-id="${task.projectId}">DETAILS</p>
                 <p class="task-date">${formatTaskDate(task.dueDate)}</p>
                 <img
                   src="${imgEdit}"
@@ -232,9 +247,13 @@ export function displayTasksInWeek() {
                  </div>
               `;
 
-        tasksContainer.appendChild(taskMenu);
-      });
-    }
+      if (task.isDone) {
+        taskMenu.classList.add("checked");
+      }
+
+      tasksContainer.appendChild(taskMenu);
+    });
+  }
   if (!tasksContainer.hasChildNodes()) {
     tasksContainer.innerHTML = "<p>No tasks available for the next week.</p>";
   }
@@ -244,11 +263,13 @@ export function displayTasksInDay() {
   const tasksContainer = document.querySelector(".task-container");
   tasksContainer.innerHTML = "";
 
-  allProjects.forEach((project) => {
-    const weeklyTasks = tasksInDay(project.tasks);
+  const projects = getProjectsFromLocalStorage();
 
-    if (weeklyTasks.length > 0) {
-      weeklyTasks.forEach((task) => {
+  projects.forEach((project) => {
+    const dailyTasks = tasksInDay(project.tasks);
+
+    if (dailyTasks.length > 0) {
+      dailyTasks.forEach((task) => {
         const taskMenu = document.createElement("div");
         taskMenu.className = "task-menu";
         taskMenu.setAttribute("data-project-id", project.id);
@@ -266,14 +287,20 @@ export function displayTasksInDay() {
         taskMenu.style.borderLeft = `5px solid ${priorityColor}`;
 
         taskMenu.innerHTML = `<div class="task-first-block">
-                <input type="checkbox" id="task-done" class="task-done" name="task-done"  task-id="${task.taskId}"
-                  data-project-id="${project.id}" />
-                <p class="task-title">${task.title}</p>
+                <input type="checkbox" id="task-done" class="task-done ${
+                  task.isDone ? "checked" : ""
+                }" name="task-done"  task-id="${task.taskId}"
+                  data-project-id="${project.id}" ${
+          task.isDone ? "checked" : ""
+        } />
+                <p class="task-title ${task.isDone ? "checked" : ""}">${
+          task.title
+        }</p>
               </div>
               <div class="task-second-block">
-                <p class="task-details" task-id="${task.taskId}" data-project-id="${
-          project.id
-        }">DETAILS</p>
+                <p class="task-details" task-id="${
+                  task.taskId
+                }" data-project-id="${project.id}">DETAILS</p>
                 <p class="task-date">${formatTaskDate(task.dueDate)}</p>
                 <img
                   src="${imgEdit}"
@@ -295,6 +322,10 @@ export function displayTasksInDay() {
                 />
                  </div>
               `;
+
+        if (task.isDone) {
+          taskMenu.classList.add("checked");
+        }
 
         tasksContainer.appendChild(taskMenu);
       });
